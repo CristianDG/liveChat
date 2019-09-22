bc1a53d830052ac9cd9b4fa8e18b679de95362a5
Cristian Di Giovanni <dgcristian@hotmail.it>
2019-09-22 18:20:13 -0300

    some serverside work

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var port = 3000;
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function(req, res) {
+    res.sendFile('index.html', { root: `${__dirname}/src` });
+});
+
+http.listen(port, () => console.log('listening on port:', port));
